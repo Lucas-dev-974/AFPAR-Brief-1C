51f1d7d45f06d635c5edba4aaf54ecbd0f3c5281
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-22 12:48:52 +0100

    Site app, handle authentification url with authentification APIView

---
+++ AFPAR/Site/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from Site.views import *
+
+urlpatterns = [
+    path('hello/', HelloView.as_view(), name="hello"),
+
+    path('auth/login', Authentification.as_view(), name="authentification")
+]
