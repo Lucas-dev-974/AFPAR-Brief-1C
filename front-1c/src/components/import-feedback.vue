c4d024a3e4bd784723e567341c54a71233ca1c59
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-01 12:39:46 +0100

    ajout de la card import/feedback et de ses composant

---
+++ front-1c/src/components/import-feedback.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-card color="gray" variant='tonal' id="importFeedback_card" class="pa-3">
+        <div class="d-flex justify-space-between">
+            <p>Feedback import:</p>
+            <v-btn color="#1E1E1E" class="text-white" size="small" @click='openFile'>Import</v-btn>
+        </div>
+
+        <div id="feedback_container">
+            d
+        </div>
+    </v-card>
+</template>
+
+
+<script src="./js/import-feedback.js"></script>
