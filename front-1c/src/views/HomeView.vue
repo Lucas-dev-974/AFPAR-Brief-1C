faa4bc031c902202fce0b6a853eb736e2652f05e
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-14 07:14:50 +0100

    HomeView ajout des composant Profile, ImportFeedbac et Graph

--- front-1c/src/views/HomeView.vue
+++ front-1c/src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
-  <main>
-    home page
+  <main class="pt-6 mx-auto container">
+    <div class="d-flex w-100 justify-space-between flex-wrap">
+      <div id="profilCard_cotainer">
+        <profil />
+      </div>
+      <div id="feedbackImport_container">
+        <importFeedback />         
+      </div>
+    </div>
+
+    <div class="mt-5">
+      <Graph />
+    </div>
+    
   </main>
 </template>
 
