bf4a6f19aa00e320dbf69f50f186f08d6da7015b
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-14 07:15:47 +0100

    home.js implétentations des composants Profile, ImportFeedBack et Graph

--- front-1c/src/views/js/home.js
+++ front-1c/src/views/js/home.js
@@ -1,4 +1,12 @@
+import profil from "../../components/profil.vue"
+import importFeedback from "@/components/import-feedback.vue"
+import Graph from '../../components/graph-section.vue'
+
 export default{
+    components: {
+        profil, importFeedback, Graph
+    },
+
     data(){
         return {
 
