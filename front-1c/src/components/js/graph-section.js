bad51911d8d9d12eb5848f3917ef22e30160682a
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-22 11:59:22 +0100

    code review

--- front-1c/src/components/js/graph-section.js
+++ front-1c/src/components/js/graph-section.js
@@ -1,10 +1,13 @@
 import request from '../../services/fetch.js'
 import Chart, { TimeScale }  from 'chart.js/auto'
 import PieChart from '../PieChart.vue'
+import filterBar from '../filter-bar.vue'
+
+import barChart from '../bar-chart.vue'
 
 export default {
     components: {
-        PieChart
+        PieChart, filterBar, barChart
     },
 
     data(){
@@ -27,20 +30,21 @@ export default {
             },
 
             chart: null,
+            pie_charte: null,
 
             showGrid: true,
             chart_title: '',
 
             nb_items: 10,
-            years: null
+            years: null,
+
+            products_for_region: []
         }
     },
 
     mounted(){
-        
         this.getYears()
         this.search()
-        
     },
 
     methods: {
@@ -55,7 +59,7 @@ export default {
                     url = '/api/regions/top'
                     params['regions'] = this.data_options.regions
                 }
-                else   {
+                else{
                     url = '/api/regions'
                     params['regions'] = this.data_options.regions
                 }
@@ -63,28 +67,29 @@ export default {
                 this.chart_title = 'Ventes par produits'
                 if(this.data_options.products.length == 0)
                     url = '/api/products/top'
-                else   {
+                else{
                     url = '/api/products'
                     params['products'] = this.data_options.regions
                 }
             }else if(this.on_graph == 'regions_products'){
                 this.chart_title = 'Région'
                 if(this.data_options.regions.length > 0){
-                    params['regions'] = tihs.data_options.regions
+                    params['regions'] = this.data_options.regions
                 }
 
                 url = '/api/regions/products'
             }
             
-            params['nb_items'] = this.nb_items
+            
             if(this.data_options.year != null)
                 params['year'] = this.data_options.year
-
+                
+            params['nb_items'] = this.nb_items
             this.getData(url, params)
         },
 
         getPays: async function(){
-            const response    = await request.get('/api/paès du servys')
+            const response    = await request.get('/api/pays')
             this.select.items = response
         },
 
@@ -105,11 +110,13 @@ export default {
 
         byRegionsProducts: function(){
             this.on_graph = 'regions_products'
+            this.search()
         },
 
         getData: async function(url, params){
             const response = await request.get(url, params)
             this.response  = response
+            
 
             if(!this.data_loaded && response != false){
                 this.buildChart()
@@ -130,6 +137,8 @@ export default {
         },
 
         configChart: function(){
+            const chart = this.chart
+
             return {
                 type: 'bar',
                 data: this.dataset(),
@@ -160,17 +169,42 @@ export default {
                               display: false
                             }
                         }
-                    }
+                    },
+
+                    onClick: this.clickHandler
 
                 },
             };
         },
 
+        clickHandler: function(evt){
+            const points = this.chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
+
+            if (points.length && this.on_graph == 'regions_products') {
+                const firstPoint = points[0];
+                const label = this.chart.data.labels[firstPoint.index];
+                const value = this.chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index]; 
+
+                this.pieChart(label)
+            }
+        },
+
         dataset: function(){
+            let datas  = Object.values(this.response)
+            let labels = Object.keys(this.response)
+
+            if(this.on_graph == 'regions_products'){
+                datas  = Object.values(this.response)
+                labels = Object.keys(this.response)
+
+                this.data_options.regions = labels
+                datas = datas.map((data) =>  data['total_sold'] )
+            }
+
             return {
                 labels: Object.keys(this.response),
                 datasets: [{
-                    data: Object.values(this.response),
+                    data: datas,
                     maxBarThickness: 50
                 }]
             }
@@ -180,5 +214,30 @@ export default {
             this.$refs.datepicker.activePicker  = 'YEAR'
         },
 
+
+        pieChart: function(label){
+            if(this.pie_charte != null) this.pie_charte.destroy()
+
+            const canvas = document.getElementById('pie_chart')
+            
+            console.log(label);
+            this.pie_charte = new Chart(canvas, {
+                type: 'pie',
+                data: this.pieChartSetup(label)
+            })
+        },
+
+        pieChartSetup: function(label){
+            const data = this.response[label].products
+
+            console.log(data);
+            return {
+                labels: data.map(product => product.stock_code),
+                datasets: [{
+                  data: data.map(product => product.invoice_detail),
+                  hoverOffset: 5
+                }]
+            };
+        }
     }
 }
